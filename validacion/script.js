de8ec660911f077b6bc578fbcd2eb91d0f6a2703
ermaseguridad-rgb <ermaseguridad@gmail.com>
2026-08-01 15:09:58 -0600

    script.js

--- validacion/script.js
+++ validacion/script.js
@@ -1,10 +1,20 @@
+const params = new URLSearchParams(window.location.search);
+const folioBuscado = params.get("folio");
+
 fetch("recibos.json")
-.then(response => response.json())
-.then(datos => {const recibo = datos[0];
-document.getElementById("folio").textContent = recibo.folio;
-document.getElementById("empleado").textContent = recibo.empleado;
-document.getElementById("puesto").textContent = recibo.puesto;
-document.getElementById("periodo").textContent = recibo.periodo;
-document.getElementById("importe").textContent = recibo.importe;}
-})
-.catch(error => console.log(error));
+  .then(response => response.json())
+  .then(datos => {
+
+    const recibo = datos.find(r => r.folio === folioBuscado);
+
+    if (!recibo) {
+      alert("Recibo no encontrado");
+      return;
+    }
+
+    document.getElementById("folio").textContent = recibo.folio;
+    document.getElementById("periodo").textContent = recibo.periodo;
+    document.getElementById("importe").textContent = recibo.importe;
+
+  })
+  .catch(error => console.log(error));
