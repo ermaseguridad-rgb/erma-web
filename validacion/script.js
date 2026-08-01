c291f5a0be439584d65141213c2dc7deb26d8b0e
ermaseguridad-rgb <ermaseguridad@gmail.com>
2026-08-01 01:37:10 -0600

    script.js

--- validacion/script.js
+++ validacion/script.js
@@ -1,1 +1,10 @@
-
+fetch("recibos.json")
+.then(response => response.json())
+.then(datos => {const recibo = datos[0];
+document.getElementById("folio").textContent = recibo.folio;
+document.getElementById("empleado").textContent = recibo.empleado;
+document.getElementById("puesto").textContent = recibo.puesto;
+document.getElementById("periodo").textContent = recibo.periodo;
+document.getElementById("importe").textContent = recibo.importe;}
+})
+.catch(error => console.log(error));
