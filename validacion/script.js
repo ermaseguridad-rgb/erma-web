3271ba4c04a34e0e7cd195e2471d10603fbfbc54
ermaseguridad-rgb <ermaseguridad@gmail.com>
2026-08-01 19:31:23 -0600

    Update script.js

--- validacion/script.js
+++ validacion/script.js
@@ -1,6 +1,6 @@
 const params = new URLSearchParams(window.location.search);
 const folioBuscado = params.get("folio");
-alert(Script cargado");
+
 fetch("recibos.json")
   .then(response => response.json())
   .then(datos => {
@@ -12,21 +12,23 @@ fetch("recibos.json")
       return;
     }
 
-document.getElementById("folio").textContent = recibo.folio;
-document.getElementById("cliente").textContent = recibo.cliente;
-document.getElementById("concepto").textContent = recibo.concepto;
-document.getElementById("periodo").textContent = recibo.periodo;
-document.getElementById("importe").textContent = recibo.importe;
-document.getElementById("formaPago").textContent = recibo.formaPago;
-document.getElementById("fecha").textContent = recibo.fecha;
-document.getElementById("codigo").textContent = recibo.codigo;
-document.getElementById("estado").textContent = recibo.estado;
-  })
-  .catch(error => console.log(error));
-document.getElementById("btnSitio").addEventListener("click", () => {
-    window.open("https://ermaseguridad-rgb.github.io/erma-web/", "_blank");
-});
+    document.getElementById("folio").textContent = recibo.folio;
+    document.getElementById("cliente").textContent = recibo.cliente;
+    document.getElementById("concepto").textContent = recibo.concepto;
+    document.getElementById("periodo").textContent = recibo.periodo;
+    document.getElementById("importe").textContent = recibo.importe;
+    document.getElementById("formaPago").textContent = recibo.formaPago;
+    document.getElementById("fecha").textContent = recibo.fecha;
+    document.getElementById("codigo").textContent = recibo.codigo;
+    document.getElementById("estado").textContent = recibo.estado;
 
-document.getElementById("btnPDF").addEventListener("click", () => {
-    window.print();
-});
+    document.getElementById("btnSitio").onclick = () => {
+        window.open("https://ermaseguridad-rgb.github.io/erma-web/","_blank");
+    };
+
+    document.getElementById("btnPDF").onclick = () => {
+        window.print();
+    };
+
+  })
+  .catch(error => console.error(error));
