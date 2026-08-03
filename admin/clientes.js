4630a14b39b5ac02821c28d62b0ae138911458c5
ermaseguridad-rgb <ermaseguridad@gmail.com>
2026-08-03 03:46:20 -0600

    Update clientes.js

--- admin/clientes.js
+++ admin/clientes.js
@@ -1,1 +1,25 @@
+const btnNuevo = document.getElementById("btnNuevo");
+const modal = document.getElementById("modalCliente");
+const cerrar = document.getElementById("cerrarModal");
 
+btnNuevo.onclick = () => {
+
+    modal.style.display = "block";
+
+};
+
+cerrar.onclick = () => {
+
+    modal.style.display = "none";
+
+};
+
+window.onclick = (e)=>{
+
+    if(e.target==modal){
+
+        modal.style.display="none";
+
+    }
+
+};
