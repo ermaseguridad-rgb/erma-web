f643509b2f883f5edd457ebdb55c48496855da8f
ermaseguridad-rgb <ermaseguridad@gmail.com>
2026-08-03 05:13:17 -0600

    Update clientes.js

--- admin/clientes.js
+++ admin/clientes.js
@@ -1,3 +1,10 @@
+import { db } from "../firebase.js";
+
+import {
+    collection,
+    addDoc
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+
 // ==========================================
 // ERMA OS
 // Módulo Clientes
@@ -41,10 +48,46 @@ window.onload = function () {
 
     const btnGuardar = document.getElementById("guardarCliente");
 
-    if (btnGuardar) {
+   if (btnGuardar) {
 
-        
-    }
+    btnGuardar.addEventListener("click", async () => {
+
+        const nombre = document.getElementById("nombre").value;
+        const empresa = document.getElementById("empresa").value;
+        const telefono = document.getElementById("telefono").value;
+        const correo = document.getElementById("correo").value;
+        const direccion = document.getElementById("direccion").value;
+        const estado = document.getElementById("estado").value;
+
+        try {
+
+            await addDoc(collection(db, "clientes"), {
+
+                nombre,
+                empresa,
+                telefono,
+                correo,
+                direccion,
+                estado,
+                fechaRegistro: new Date()
+
+            });
+
+            alert("✅ Cliente guardado correctamente.");
+
+            modal.style.display = "none";
+
+        } catch (error) {
+
+            console.error(error);
+
+            alert("Error al guardar.");
+
+        }
+
+    });
+
+}
 
     console.log("✅ ERMA OS - Clientes iniciado correctamente");
 
