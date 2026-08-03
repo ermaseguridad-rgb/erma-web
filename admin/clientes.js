950ba10a2e521274deb5ae896098a161ab012f9e
ermaseguridad-rgb <ermaseguridad@gmail.com>
2026-08-03 05:46:40 -0600

    Update clientes.js

--- admin/clientes.js
+++ admin/clientes.js
@@ -3,7 +3,9 @@ import { db } from "../firebase.js/firebase.js";
 import {
     collection,
     addDoc,
-    getDocs
+    getDocs,
+    deleteDoc,
+    doc
 } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 // ==========================================
